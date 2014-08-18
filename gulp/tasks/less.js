277b4a0d60c4deb9ec5e82940c57ffc2ccdf3456
Pierre <mfpierre@gmail.com>
2014-08-18 15:15:51 +0200

    Use semantic grid instead of semantic ui

--- gulp/tasks/less.js
+++ gulp/tasks/less.js
@@ -3,6 +3,9 @@ var less = require('gulp-less');
 
 gulp.task('less', function () {
   gulp.src('./src/less/*.less')
-    .pipe(less())
-    .pipe(gulp.dest('./build/css'));
+    .pipe(less({
+        compress:true,
+        paths: [ '../../src/less/includes' ]
+    }))
+    .pipe(gulp.dest('./build'));
 });
