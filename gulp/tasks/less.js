97905dfbe80fc04afa61f5ecd2580375a7bbd178
Pierre <mfpierre@gmail.com>
2014-11-17 16:01:20 +0100

    Add editor config

--- gulp/tasks/less.js
+++ gulp/tasks/less.js
@@ -3,9 +3,9 @@ var less = require('gulp-less');
 
 gulp.task('less', function () {
   gulp.src('./src/less/*.less')
-    .pipe(less({
-        compress:true,
-        paths: [ '../../src/less/includes' ]
-    }))
-    .pipe(gulp.dest('./build'));
+  .pipe(less({
+    compress:true,
+    paths: [ '../../src/less/includes' ]
+  }))
+  .pipe(gulp.dest('./build'));
 });
