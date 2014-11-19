4fe57bdf72f16aef9ec734fe10c92bb051174a47
Pierre <mfpierre@gmail.com>
2014-11-19 17:30:30 +0100

    Handle jade compilation errors

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var handleErrors = require('../util/handleErrors');
 
 gulp.task('markup', function() {
-  return gulp.src('src/templates/*.jade')
-    .pipe(jade({
-      pretty: false
-    }))
-    .pipe(gulp.dest('build'));
+  gulp.src('src/templates/*.jade')
+  .pipe(jade({
+    pretty: false
+  }))
+  .on('error', handleErrors)
+  .pipe(gulp.dest('build'));
 });
